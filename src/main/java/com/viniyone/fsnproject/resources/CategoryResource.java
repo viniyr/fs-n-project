11d4d66cf3e42cdc9711da2a79db1cedd643672e
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-08-09 22:10:17 -0300

    Testing REST

---
+++ src/main/java/com/viniyone/fsnproject/resources/CategoryResource.java
@@ -0,0 +1,16 @@
+package com.viniyone.fsnproject.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listing() {
+		return "Working REST";
+	}
+	
+}
