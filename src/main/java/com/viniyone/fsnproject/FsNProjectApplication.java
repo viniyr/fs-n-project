8c28997b0c5988a811591b5adcd10ae3ae627488
Vinicius Yonezawa <vinicius.yonezawa@gmail.com>
2021-08-09 22:00:07 -0300

    Project Created

---
+++ src/main/java/com/viniyone/fsnproject/FsNProjectApplication.java
@@ -0,0 +1,13 @@
+package com.viniyone.fsnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsNProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsNProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniyone/fsnproject/FsNProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniyone.fsnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsNProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
