526d694d314dd297af7aeb3c014e307449ab0c1f
viniyr <vinicius.yonezawa@gmail.com>
2021-08-11 21:49:01 -0300

    Endpoint adjustments

---
+++ src/main/java/com/viniyone/fsnproject/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.viniyone.fsnproject.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.viniyone.fsnproject.services.exceptions.ObjectNotFoundException;
+import com.viniyone.fsnproject.services.exceptions.StandardError;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) { 
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+	
+}
--- src/main/java/com/viniyone/fsnproject/services/CategoryService.java
+++ src/main/java/com/viniyone/fsnproject/services/CategoryService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.viniyone.fsnproject.domain.Category;
 import com.viniyone.fsnproject.repositories.CategoryRepository;
+import com.viniyone.fsnproject.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repo;
-	
+
 	public Category find(Integer id) { 
 		  Optional<Category> obj = repo.findById(id); 
-		  return obj.orElse(null); 
+		  return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		      "Object not found! Id: " + id + ", Type: " + Category.class.getName())); 
 		} 
 }
