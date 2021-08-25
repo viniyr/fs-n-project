c056031802224d36acf109e5280abbf27f4f007b
viniyr <vinicius.yonezawa@gmail.com>
2021-08-24 22:00:29 -0300

    Customizing error validation

---
+++ src/main/java/com/viniyone/fsnproject/resources/exceptions/ValidationError.java
@@ -0,0 +1,28 @@
+package com.viniyone.fsnproject.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.viniyone.fsnproject.services.exceptions.StandardError;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+	
+
+}
