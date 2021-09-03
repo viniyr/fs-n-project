a85c2e502bdc8a2a2a483b3023488ef68ac0fc2f
viniyr <vinicius.yonezawa@gmail.com>
2021-09-02 23:19:03 -0300

    Implementing Authorization

--- src/main/java/com/viniyone/fsnproject/config/SecurityConfig.java
+++ src/main/java/com/viniyone/fsnproject/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.viniyone.fsnproject.security.JWTAuthenticationFilter;
+import com.viniyone.fsnproject.security.JWTAuthorizationFilter;
 import com.viniyone.fsnproject.security.JWTUtil;
 
 
@@ -41,7 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private static final String[] PUBLIC_MATCHERS_GET = { 
 			"/products/**",
-			"/categories/**"
+			"/categories/**",
+			"/customers/**"
 	};
 	
 	@Override
@@ -57,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
