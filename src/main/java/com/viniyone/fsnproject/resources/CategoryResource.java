9005ea1756a11eeb5cfa3515a5ce9758f207274f
viniyr <vinicius.yonezawa@gmail.com>
2021-09-06 23:34:06 -0300

    Endpoint authorization for specific profiles

--- src/main/java/com/viniyone/fsnproject/resources/CategoryResource.java
+++ src/main/java/com/viniyone/fsnproject/resources/CategoryResource.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,7 @@ public class CategoryResource {
 		
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoryDTO objDto) { 
 		Category obj = service.fromDTO(objDto);
@@ -44,6 +46,7 @@ public class CategoryResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoryDTO objDto, @PathVariable Integer id) { 
 		Category obj = service.fromDTO(objDto);
@@ -52,6 +55,7 @@ public class CategoryResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	  public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
