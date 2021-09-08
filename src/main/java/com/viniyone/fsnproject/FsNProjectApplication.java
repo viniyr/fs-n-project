c0dc326195b4904d9192c4abb41ed4a1d4dd6fab
viniyr <vinicius.yonezawa@gmail.com>
2021-09-08 19:34:37 -0300

    Saving first file in AWS S3

--- src/main/java/com/viniyone/fsnproject/FsNProjectApplication.java
+++ src/main/java/com/viniyone/fsnproject/FsNProjectApplication.java
@@ -1,12 +1,17 @@
 package com.viniyone.fsnproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.viniyone.fsnproject.services.S3Service;
+
 @SpringBootApplication
 public class FsNProjectApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FsNProjectApplication.class, args);
@@ -14,6 +19,6 @@ public class FsNProjectApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\tmp\\rack.jpg");
 	}
 }
---
+++ src/main/java/com/viniyone/fsnproject/services/S3Service.java
@@ -0,0 +1,45 @@
+package com.viniyone.fsnproject.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+
+@Service
+public class S3Service {
+
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private AmazonS3 s3client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	public void uploadFile(String localFilePath) { 
+		
+		try {
+			File file = new File(localFilePath);
+			LOG.info("Initializing");
+			s3client.putObject(new PutObjectRequest(bucketName, "rack.jpg", file));
+			LOG.info("DONE!");
+		} catch (AmazonServiceException e) {
+			LOG.info("AmazonServiceException: " + e.getErrorMessage());
+			LOG.info("Status code: " + e.getErrorCode());
+			//e.printStackTrace();
+		} catch (AmazonClientException e) {
+			LOG.info("AmazonClientException: " + e.getMessage());
+		}
+		
+	}
+	
+}
